1c31102b21a20bae0c88c4d52142b01e5916de72
Alex Trottier <postables@rtradetechnologies.com>
2020-01-10 21:55:45 -0800

    chore: cleanup

--- example/main.go
+++ example/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/biezhi/gorm-paginator/pagination"
+	"github.com/RTradeLtd/gpaginator"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
